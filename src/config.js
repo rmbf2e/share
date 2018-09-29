dfdc58d7de840435bf72039c977ee0bea3a6c178
yush <389636851@qq.com>
2018-09-29 18:03:25 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,6 +6,9 @@ module.exports = {
   loginHost: `${process.env.LOGIN_HOST === 'test' ? 'http' : 'https'}://${
     process.env.LOGIN_HOST === 'test' ? 'test.' : ''
   }ssa.jd.com/sso/login`,
+  logoutHost: `${process.env.LOGIN_HOST === 'test' ? 'http' : 'https'}://${
+    process.env.LOGIN_HOST === 'test' ? 'test.' : ''
+  }ssa.jd.com/sso/logout`,
   pageSizeOptions: ['20', '30', '50'],
 
   // sider默认折叠
